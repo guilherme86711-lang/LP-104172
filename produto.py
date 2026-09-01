958daf5c9ca091c8743b58ab4c70800b01aa61f1
guilherme86711-lang <guilherme86711@gmail.com>
2026-09-01 13:57:59 -0300

    Add files via upload

---
+++ produto.py
@@ -0,0 +1,20 @@
+import os
+os.system("cls")
+
+numero1 = float(input("digite um numero: "))
+numero2 = float(input("digite outro numero: "))
+
+#calculando
+
+soma= numero1 + numero2
+media = soma / 2
+produto= numero1 * numero2
+
+maior = max(numero1, numero2)
+menor = min(numero1, numero2)
+
+print("numero maior é:", maior)
+print("numero menor é:", menor)
+
+
+
